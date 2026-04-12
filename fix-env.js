cecd7d2787c6fc08657ff61f194a580cc03064cb
axwissam <axwissam@gmail.com>
2026-04-12 15:34:08 -0300

    security: remove chave SSH e scripts temporarios do git

--- fix-env.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('.env.local', 'utf8');
-c = c.replace(
-  'SUPABASE_SERVICE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzY3R4endta29haWdjYnJ2aGlxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NDczOTgxNSwiZXhwIjoyMDkwMzE1ODE1fX0.1mK4SQc6PqBr22ofyIVCo-5K2AM9kAEEVrwR7mFYp60',
-  'SUPABASE_SERVICE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzY3R4endta29haWdjYnJ2aGlxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NDczOTgxNSwiZXhwIjoyMDkwMzE1ODE1fQ.1mK4SQc6PqBr22ofyIVCo-5K2AM9kAEEVrwR7mFYp60'
-);
-fs.writeFileSync('.env.local', c);
-console.log('OK');
